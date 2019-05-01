791eee6a80ae1667a096642eb82ecd8df2aa1aac
cameron-chromiak <cam.chromiak@hotmail.com>
2019-05-01 18:59:56 -0400

    added sign in/out buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -12,20 +12,41 @@ class GoogleAuth extends Component{
       window.gapi.client.init({
         client_id: '951358901456-m6n9rac87saoti5sogibhopudhi60137.apps.googleusercontent.com',
         scope: 'email'
-      }).then( res =>{
+      }).then( () =>{
         this.auth = window.gapi.auth2.getAuthInstance()
         this.setState({isSignedIn: this.auth.isSignedIn.get()})
+        this.auth.isSignedIn.listen(this.onAuthChange)
       })
     })
   }
 
+  onAuthChange = () =>{
+    this.setState({isSignedIn: this.auth.isSignedIn.get()})
+  }
+
+  onSignInClick = () =>{
+    this.auth.signIn()
+  }
+
+  onSignOutClick = () =>{
+    this.auth.signOut()
+  }
+
   renderAuthButton(){
     if(this.state.isSignedIn === null){
-       return <div>IDK</div>
+       return null
     }else if(this.state.isSignedIn){
-        return  <div>Signed in</div>
+        return (
+          <button onClick={this.onSignOutClick} className='ui red google button'>
+            <i className='google icon'/>Sign Out
+          </button>
+        )
     }else{
-      return <div>Not signed in</div>
+      return (
+        <button onClick={this.onSignInClick} className='ui red google button'>
+          <i className='google icon'/>Sign In
+        </button>
+      )
     }
   }
 
