f67d088d1bdc5f43fc674504fd4884fd582eb077
cameron-chromiak <cam.chromiak@hotmail.com>
2019-05-02 14:29:57 -0400

    -added actions and reducers for CRUD opps -add json-server -videos render in list on homepage

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,6 @@
+import {CREATE_STREAM, FETCH_STREAM, DELETE_STREAM, FETCH_STREAMS, EDIT_STREAM} from './types'
+import streams from '../apis/streams'
+
 export const signIn = (userId) =>{
   return{
     type: 'SIGN_IN',
@@ -9,4 +12,45 @@ export const signOut = () =>{
   return{
     type: 'SIGN_OUT'
   }
+}
+
+export const createStream = (formValues) => async dispatch => {
+  const res = await streams.post('/streams', formValues)
+  dispatch({
+    type: CREATE_STREAM,
+    payload: res.data
+  })
+}
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streams.get('/streams')
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: res.data
+  })
+}
+
+export const fetchSream = (id) => async dispatch=>{
+  const res = await streams.get(`/streams/${id}`)
+  dispatch({
+    type: FETCH_STREAM,
+    payload: res.data
+  })
+}
+
+const editStream = (id, formValues) => async dispatch => {
+  const res = await streams.put(`/streams/${id}`, formValues)
+  dispatch({
+    type: EDIT_STREAM,
+    payload: res.data
+  })
+}
+
+const deleteStream = (id) => async dispatch =>{
+  await streams.delete(`/streams/${id}`)
+
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
+  })
 }
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { Field, reduxForm } from 'redux-form'
-
-
+import {connect} from 'react-redux'
+import {createStream} from '../../actions/index'
 class StreamCreate extends Component{
 
 
@@ -26,8 +26,8 @@ class StreamCreate extends Component{
     )
   }
 
-  onSubmit(formValues){
-    console.log(formValues);
+  onSubmit = (formValues) =>{
+    this.props.createStream(formValues)
   }
 
 
@@ -56,7 +56,10 @@ const validate = (formValues) =>{
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate,
 })(StreamCreate)
+
+
+export default connect(null, {createStream})(formWrapped)
